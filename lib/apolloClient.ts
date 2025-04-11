2e3b24fba59ddd77c6535a044addf653b33dc18f
chewybluepen <136284345+chewybluepen@users.noreply.github.com>
2025-04-11 00:05:55 -0500

    Update apolloClient.ts

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -1,11 +1,13 @@
+// lib/apolloClient.ts
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT, // ensure you have this environment variable set
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT, // Ensure this environment variable is set
   cache: new InMemoryCache({
     typePolicies: {
       User: {
-        keyFields: ["id"],
+        // Disable automatic merging of User objects to bypass the missing ID error
+        keyFields: false,
       },
     },
   }),
