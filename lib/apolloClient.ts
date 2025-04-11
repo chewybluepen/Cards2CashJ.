ed30b6cd33a485a16583739c7643fdef99aeda7c
chewybluepen <136284345+chewybluepen@users.noreply.github.com>
2025-04-11 09:35:34 -0500

    Update apolloClient.ts

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -1,13 +1,17 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-// DEMO MODE ONLY: Turn off normalization, allow duplicates, ignore ID warnings
+// DEMO MODE ONLY: Completely disable cache normalization and error reporting.
+// This configuration forces Apollo to never try to merge objects based on IDs.
+
 const client = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || "http://localhost:4000/graphql",
   cache: new InMemoryCache({
+    // Disable caching for User objects and for the Query field "user".
     typePolicies: {
       Query: {
         fields: {
           user: {
+            // Always replace the "user" data with the incoming data and ignore existing data.
             merge(existing, incoming) {
               return incoming;
             },
@@ -15,24 +19,27 @@ const client = new ApolloClient({
         },
       },
       User: {
-        keyFields: false, // Don't expect an ID
+        // Do not expect any ID (i.e. keyFields are false)
+        keyFields: false,
+        // Always override existing User data with incoming data.
         merge(existing, incoming) {
-          return incoming; // Always replace, never merge
+          return incoming;
         },
       },
     },
   }),
   defaultOptions: {
+    // Disable caching and ignore errors for queries and mutations.
     watchQuery: {
-      fetchPolicy: 'no-cache',
-      errorPolicy: 'ignore',
+      fetchPolicy: "no-cache",
+      errorPolicy: "ignore",
     },
     query: {
-      fetchPolicy: 'no-cache',
-      errorPolicy: 'ignore',
+      fetchPolicy: "no-cache",
+      errorPolicy: "ignore",
     },
     mutate: {
-      errorPolicy: 'ignore',
+      errorPolicy: "ignore",
     },
   },
 });
